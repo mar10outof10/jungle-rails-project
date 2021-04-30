ef3b91dfb8ddafee07857c7740b59c11da6b18f2
Martin Halas <echoes.time@gmail.com>
2021-04-30 03:06:50 +0000

    Added about page and added to nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
+  resources :about, only: [:index]
 
   resource :cart, only: [:show] do
     post   :add_item
@@ -11,6 +12,7 @@ Rails.application.routes.draw do
   end
 
   resources :orders, only: [:create, :show]
+
 
   namespace :admin do
     root to: 'dashboard#show'
