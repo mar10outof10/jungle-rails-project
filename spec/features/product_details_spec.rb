c45e94806f3a993f57a7af342b9216e03b62e266
Martin Halas <echoes.time@gmail.com>
2021-05-04 22:32:40 +0000

    Added feature spec for product details when clicking product link

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
-RSpec.feature "Visitor can navigate from the home page to the product detail page by clicking on a product", type: :feature, js: true do
+RSpec.feature "Visitor navigates to product page", type: :feature, js: true do
+  # SETUP
   before :each do
     @category = Category.create! name: 'Apparel'
 
@@ -12,7 +13,26 @@ RSpec.feature "Visitor can navigate from the home page to the product detail pag
         price: 64.99
       )
     end
+    
   end
 
+  scenario "They can navigate to the product detail page by clicking on a product" do
 
+    @category.products.create!(
+      name: 'Blue Jeans',
+      description: Faker::Hipster.paragraph(4),
+      image: open_asset('apparel1.jpg'),
+      quantity: 10,
+      price: 64.99
+    )
+
+    visit root_path
+
+    click_on 'Blue Jeans'
+    
+    sleep 1
+
+    expect(page).to have_css '.product-detail'
+
+  end
 end
