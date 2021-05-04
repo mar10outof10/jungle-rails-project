ebff2daf0323940bd670a04630d9efc2e12e0537
Martin Halas <echoes.time@gmail.com>
2021-05-04 00:36:49 +0000

    Fixed user form validation

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe "Validations" do
+    it "should be created with two matching password and password_confirmation fields"
+  end
+
+  describe "Passwords" do
+
+  end
+
+  describe "Authentication (class) method" do
+    
+  end
+end
