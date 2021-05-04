45cc6af12487c3700084f81c4d553ec04ff0f2d3
Martin Halas <echoes.time@gmail.com>
2021-05-04 00:05:41 +0000

    Added all validation tests for create method of product object

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,15 +3,35 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
 
   describe 'Validations' do
-    it 'should create a product if all forms are filled out correctly' do
-      @category = Category.new(:name => "Electronics")
-      @product = Product.new(:name => "Lamp", :price => "50", :quantity => "3", :category => @category)
-      expect(@product).to be_present
+
+    before do
+      @category = Category.new(:name =>"Electronics")
     end
 
-    # it 'should validate that a name exists when a product is created' do
-    #   expect(@product.name).to be_present
-    # end
+    it 'should create a product if all forms are filled out correctly' do
+      @product = Product.create(:name => "Lamp", :price => "50", :quantity => "3", :category => @category)
+      expect(@product.errors.full_messages).to match []
+    end
+
+    it 'should not create a product if the name field is left empty' do
+      @productMissingAttr = Product.create(:name => nil, :price => "50", :quantity => "3", :category => @category)
+      expect(@productMissingAttr.errors.full_messages[0]).to match "Name can't be blank"
+    end
+
+    it 'should not create a product if the price field is left empty' do
+      @productMissingAttr = Product.create(:name => "Lamp", :price => nil, :quantity => "3", :category => @category)
+      expect(@productMissingAttr.errors.full_messages[0]).to match "Price cents is not a number"
+    end
+
+    it 'should not create a product if the quantity field is left empty' do
+      @productMissingAttr = Product.create(:name => "Lamp", :price => "50", :quantity => nil, :category => @category)
+      expect(@productMissingAttr.errors.full_messages[0]).to match "Quantity can't be blank"
+    end
+
+    it 'should not create a product if the category is left empty' do
+      @productMissingAttr = Product.create(:name => "Lamp", :price => "50", :quantity => "3", :category => nil)
+      expect(@productMissingAttr.errors.full_messages[0]).to match "Category can't be blank"
+    end
 
   end
 end
