47b500857caa2429710406c231d5e76f1d976371
Martin Halas <echoes.time@gmail.com>
2021-05-04 22:41:32 +0000

    Added capybara feature spec fora dding a product to the cart

---
+++ spec/features/add_to_cart_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor adds a product to the cart", type: :feature, js: true do
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+    
+  end
+
+  scenario "Adding a product" do
+
+    @category.products.create!(
+      name:  Faker::Hipster.sentence(3),
+      description: Faker::Hipster.paragraph(4),
+      image: open_asset('apparel1.jpg'),
+      quantity: 10,
+      price: 64.99
+    )
+
+    visit root_path
+
+    click_on('Add', match: :first)
+
+    expect(page).to have_text 'My Cart (1)'
+
+  end
+
+end
