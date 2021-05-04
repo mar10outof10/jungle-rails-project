aa66299a0c633c71b32001c2e01d96d75bada501
Martin Halas <echoes.time@gmail.com>
2021-05-04 22:11:57 +0000

    Added feature spec and before :each for product_details_spec

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor can navigate from the home page to the product detail page by clicking on a product", type: :feature, js: true do
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+
+end
